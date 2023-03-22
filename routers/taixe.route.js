aa75c96246a6709dd9951c655d8b775c072ef47c
Nam <namandroid.it@gmail.com>
2023-03-22 22:22:34 +0700

    hhi

--- routers/taixe.route.js
+++ routers/taixe.route.js
@@ -5,6 +5,9 @@ module.exports = app => {
 
     route.post('/taixeAnt100Init', verifyToken, TaiXe.getChuyen);
     route.post('/taixeAnt100Updatestatusorder', verifyToken, TaiXe.Updatestatusorder);
+    route.post('/taixeAnt100Updatestatus01', verifyToken, TaiXe.Updatestatus01);
+    route.post('/taixeAnt100Insertchiphi', verifyToken, TaiXe.Insertchiphi);
+    route.post('/taixeAnt100Updatechiphi', verifyToken, TaiXe.Updatechiphi);
   
     app.use("/api/taixe",route);
 }
