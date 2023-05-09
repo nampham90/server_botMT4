94b7c15bb2e59329dc3d4a2e705082faf727f626
nampham <pv_nam@brycen.com.vn>
2023-05-09 16:55:27 +0700

    hihi

--- controller/tmt050.controller.js
+++ controller/tmt050.controller.js
@@ -5,6 +5,8 @@ let Response = Responses.Response
 // process
 const Tmt050getListKBNProcess = require("../process/tmt050Process/tmt050getListKBNProcess");
 const Spin00901DetailProcess = require("../process/spin00901Process/spin00901DetailProcess");
+const Spin00901UpdateProcess = require("../process/spin00901Process/spin00901UpdateProcess");
+const Spin00901DelProcess = require("../process/spin00901Process/spin00901DelProcess");
 
 exports.getListKBN = async (req,res) => {
     try {
@@ -34,17 +36,27 @@ exports.getDetail = async (req,res) => {
 
 exports.update = async (req,res) => {
     try {
-        
+        const spin00901UpdateProcess = new Spin00901UpdateProcess(dbCon.dbDemo);
+        await spin00901UpdateProcess.start();
+        const session = spin00901UpdateProcess.transaction;
+        let response = await spin00901UpdateProcess.update(req.body,session);
+        await spin00901UpdateProcess.commit();
+        return res.status(200).send(new Response(0,'data sucess !',response));
     } catch (error) {
-        
+        return  res.status(200).send(new Response(1001,"Lỗi hệ thống ", error.message));
     }
 }
 
 exports.delete = async (req,res) => {
     try {
-        
+        const spin00901DelProcess = new Spin00901DelProcess(dbCon.dbDemo);
+        await spin00901DelProcess.start();
+        const session = spin00901DelProcess.transaction;
+        let response = await spin00901DelProcess.delete(req.body,session);
+        await spin00901DelProcess.commit();
+        return res.status(200).send(new Response(0,'data sucess !',response));
     } catch (error) {
-        
+        return  res.status(200).send(new Response(1001,"Lỗi hệ thống ", error.message));
     }
 }
 
