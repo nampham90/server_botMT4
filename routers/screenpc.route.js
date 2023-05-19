3531764b2b5926c1ed466953cf19ec3a743c42cb
Nam <namandroid.it@gmail.com>
2023-05-20 00:25:26 +0700

    hihi

--- routers/screenpc.route.js
+++ routers/screenpc.route.js
@@ -3,8 +3,10 @@ module.exports = app => {
     const verifyToken = require('../middlewares/verifyToken');
     const route = require("express").Router();
 
-    route.post("/ant100SearchAllDatasc",Screenpc.getAllDataSC);
-    route.post("/ant100AddListDatasc",Screenpc.addListDatasc)
+    route.post("/ant100SearchAllDatasc",verifyToken,Screenpc.getAllDataSC);
+    route.post("/ant100AddListDatasc",verifyToken,Screenpc.addListDatasc);
+    route.put("/ant100EditDatasc",verifyToken,Screenpc.updateDataSC)
+    route.post("/ant100DetailDatasc",verifyToken,Screenpc.getDetailDataSC)
 
     app.use("/api/screenpc",route);
 }
