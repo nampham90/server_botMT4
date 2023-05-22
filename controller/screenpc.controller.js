30f5d7f3cff67e4c7ce7b86eea0096ac4bd652b3
nampham <pv_nam@brycen.com.vn>
2023-05-22 10:49:39 +0700

    hihi

--- controller/screenpc.controller.js
+++ controller/screenpc.controller.js
@@ -6,7 +6,6 @@ const Screenpc = db.screenpc;
 
 
 exports.getAllDataSC = async (req,res) => {
-    console.log("hihi: "+req.body);
     let n = req.body.pageNum - 1;
     let allData = await Screenpc.find(req.body.filters);
     let lst = await Screenpc.find(req.body.filters).limit(req.body.pageSize).skip(req.body.pageSize*n);
@@ -22,9 +21,41 @@ exports.addListDatasc = async (req,res) => {
             lang:element.lang,
             title1: element.title1,
             title2: element.title2 == null? "" : element.title2,
+            vitri: element.vitri,
             status: element.status
         })
         await newSceen.save();
     }
     res.status(200).send(new Response(0, "data sucess",1));
+}
+
+exports.getDetailDataSC = async (req,res) => {
+    let dataSc = await Screenpc.findOne({_id:req.body.id});
+    if(dataSc) {
+        return res.status(200).send(new Response(0,"Data sucess ", dataSc));
+    } else {
+        return res.status(200).send(new Response(0,"Data Null ", null));
+    }
+}
+
+exports.updateDataSC = async (req,res) => {
+    Screenpc.updateOne({_id:req.body.id},{$set: 
+        {
+            title1: req.body.title1,
+            title2: req.body.title2, 
+            vitri: req.body.vitri, 
+            lang: req.body.lang, 
+            status:req.body.status
+        }
+    })
+    .then(data => {
+        return res.status(200).send(new Response(0,"Data sucess ", data.modifiedCount));
+    },err => {
+        return res.status(200).send(new Response(0,"Error ",null));
+    })
+
+}
+
+exports.deletelDataSC = async (req, res) => {
+    
 }
