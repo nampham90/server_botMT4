25bea1286d93aa26a7598a9a9cab703d0f54deb2
nampham <pv_nam@brycen.com.vn>
2023-08-31 14:54:24 +0700

    hihi

--- socketService/socket.service.js
+++ socketService/socket.service.js
@@ -13,6 +13,31 @@ class SocketServices{
             socket.broadcast.emit('chat message', "server send :" + msg);
         })
 
+        socket.on('DemoListProduct', req=> {
+            console.log('server on DemoListProduct');
+            let proList = [
+                {
+                    'idpro': 1,
+                    'proname': 'iphone',
+                    'price': 1200,
+                    'completed': true
+                },
+                {
+                    'idpro': 2,
+                    'proname': 'samsung',
+                    'price': 1000,
+                    'completed': true
+                },
+                {
+                    'idpro': 3,
+                    'proname': 'mac',
+                    'price': 1500,
+                    'completed': true
+                },
+            ]
+            socket.broadcast.emit('DemoListProduct', proList);
+        })
+
         // on room..
     }
 }
