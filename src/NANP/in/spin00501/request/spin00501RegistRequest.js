3683ac90a8049397bec2ba196672dfa03a732ed8
BRYCEN-VN\lq_giau <lq_giau@brycen.com.vn>
2024-03-28 17:19:17 +0700

    20240328_add request spin00501

---
+++ src/NANP/in/spin00501/request/spin00501RegistRequest.js
@@ -0,0 +1,10 @@
+const AbstractRequest = require("../../../../common/abstract/AbstractRequest");
+
+const Joi = require('joi');
+class Spin00501RegistRequest extends AbstractRequest {
+    constructor(req) {
+        super(req)
+    }
+}
+
+module.exports = Spin00501RegistRequest;
